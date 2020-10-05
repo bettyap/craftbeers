a816773b56f1ba3ff442e49214d7f29f93a87883
bettyap <beth_ap@live.com>
2020-10-04 23:01:44 -0300

    adicionado estilo novo do layout

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@ session_start();
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
   <!--Barra de Navegador (Navbar)-->
   <nav class="navbar navbar-light bg-light">
@@ -18,47 +20,44 @@ session_start();
     <a class="btn btn-outline-success my-2 my-sm-0" href="cadastro.html">Registrar-se</a>
   </nav>
   <!--Final do navbar-->
-
-  <!--Avatar-->
-  <section class="wrapper-avatar">
-      <div class="avatar"></div>
-  </section>
-
-<!-- Formulário de Login-->
-<div class="wrapper">
-  <?php 
-    // if(isset($_SESSION['cadastro_sucesso'])):
-  ?>
-  <!-- <div class="alert alert-success" role="alert">
-    Cadastro realizado com sucesso!
-  </div> -->
-  <?php
-    // endif;
-    unset($_SESSION['cadastro_sucesso']);
-  ?>
-  <form action="login.php" method="POST" class="form-signin"> 
-    <div class="row form-field">
-      <input type="text" class="form-control" name="usuario" placeholder="Email Address" required="" autofocus="" />
-    </div> 
-    <div class="row form-field">
-      <input type="password" class="form-control" name="senha" placeholder="Password" required=""/>      
-    </div>   
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Lembre-me
-        </label>
-    <div class="row justify-content-center">
-        <button type="submit" class="btn btn-outline-primary">Login</button>  
-    </div>
-    <?php
-    if(isset($_SESSION['nao_autenticado'])):
-    ?>
-    <p>OCORREU UM ERRO USUARIO OU SENHA INVÁLIDA </p> 
-    <?php
-    endif;
-    unset($_SESSION['nao_autenticado']);
-    ?>
-</form>
-</div>
+      <!--Avatar-->
+      <section class="wrapper-avatar">
+        <div class="avatar"></div>
+      </section>
+      <!-- Formulário de Login-->
+      <div class="wrapper">
+        <?php 
+          // if(isset($_SESSION['cadastro_sucesso'])):
+        ?>
+        <!-- <div class="alert alert-success" role="alert">
+          Cadastro realizado com sucesso!
+        </div> -->
+        <?php
+          // endif;
+          unset($_SESSION['cadastro_sucesso']);
+        ?>
+          <form action="login.php" method="POST" class="form-signin"> 
+            <div class="row form-field">
+              <input type="text" class="form-control" name="usuario" placeholder="Email " required="" autofocus="" />
+            </div> 
+            <div class="row form-field">
+              <input type="password" class="form-control" name="senha" placeholder="Senha" required=""/>      
+            </div>   
+                <!-- <label class="checkbox">
+                  <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Lembre-me
+                </label> -->
+            <div class="row justify-content-center">
+                <button type="submit" class="btn btn-outline-primary">Login</button>  
+            </div>
+                <?php
+                if(isset($_SESSION['nao_autenticado'])):
+                ?>
+                <p>OCORREU UM ERRO USUARIO OU SENHA INVÁLIDA </p> 
+                <?php
+                endif;
+                unset($_SESSION['nao_autenticado']);
+                ?>
+          </form>
 <?php
 
 ?>
@@ -67,4 +66,5 @@ session_start();
   <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
+
 </html> 
--- lista-categorias.php
+++ lista-categorias.php
@@ -41,6 +41,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
   <!--Barra de Navegador (Navbar)  -->
   <nav class="navbar navbar-light bg-light">
@@ -52,32 +53,35 @@
       <a href="logout.php">Sair</a> 
   </nav>
   <!--Final do navbar-->
-  <!-- Cadastrar categoria da cerveja -->
-<div class="center-form card card-padding">
-  <h2 class="form-title">CATEGORIAS</h2>  
-  <div class="lista-categorias">
-    
-    <?php
-      foreach ($dados as $categoria) {
-    ?>
-      <a href="categoria.php?id=<?php echo $categoria['id'] ?>" class="card categoria-card">
-        <div class="card-text">
-          <?php
-            echo $categoria['descricao'];
-          ?>
+  <main class="content">
+    <div class="card card-padding">
+        <!-- Cadastrar categoria da cerveja -->
+        <div class="center-form card card-padding">
+          <h2 class="form-title">CATEGORIAS</h2>  
+            <div class="lista-categorias">            
+                <?php
+                  foreach ($dados as $categoria) {
+                ?>
+                  <a href="categoria.php?id=<?php echo $categoria['id'] ?>" class="card categoria-card">
+                    <div class="card-text">
+                      <?php
+                        echo $categoria['descricao'];
+                      ?>
+                    </div>
+                  </a>
+                <?php
+                  }
+                ?>
         </div>
-      </a>
-    <?php
-      }
-    ?>
-</div>
-<div class="row justify-content-center">
-  <a href="criar-categoria.html" class="btn btn-outline-primary">Nova Categoria</a>  
-</div>
-</div>
+      <div class="row justify-content-center">
+        <a href="criar-categoria.html" class="btn btn-outline-primary">Nova Categoria</a>  
+      </div>
+    </div>
+  </main>
 
 <!-- JQuery, Bootstrap -->
   <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
+
 </html> 
