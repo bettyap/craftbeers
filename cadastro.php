7708da4f5af8989c5907289d346f10f43e705496
bettyap <beth_ap@live.com>
2020-11-08 21:33:16 -0300

    Adicionado extensao php

--- cadastro.php
+++ cadastro.php
@@ -1,38 +1,63 @@
-<?php
+<?php 
   session_start();
-  include('conexao.php');
-
-  // verificar se os campos tem valores
-  if(
-    empty($_POST['nome']) ||
-    empty($_POST['num_telefone']) ||
-    empty($_POST['senha']) ||
-    empty($_POST['email']) ||
-    empty($_POST['data_nasc'])
-  ) {
-    header('Location: index.php');
-    exit();
-  }  
-
-  // proteção contra ataque 
-  $nome = mysqli_real_escape_string($conexao, $_POST['nome']);
-  $num_telefone = mysqli_real_escape_string($conexao, $_POST['num_telefone']);
-  $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
-  $email = mysqli_real_escape_string($conexao, $_POST['email']);
-  $data_nasc = mysqli_real_escape_string($conexao, $_POST['data_nasc']);
-
-  $query = "INSERT INTO `usuario` (`nome`, `num_telefone`, `senha`, `email`, `data_nasc`) VALUES ('{$nome}', '{$num_telefone}', md5('{$senha}'), '{$email}', '{$data_nasc}')";
-  // echo $query; exit;
-  $result = mysqli_query($conexao, $query);
-  // echo mysqli_error($conexao); exit; erro de banco ao executar $query
-  $row = mysqli_num_rows($result);
-
-  if($row == 1) {
-    $_SESSION['cadastro_sucesso'] = true;
-    header('Location: index.php');
-    exit();
-  } else {
-    header('Location: cadastro.php');
-    exit();
-  }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Craft Beers</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/botoes.css">
+</head>
+
+<body>
+  <!--Barra de Navegador (Navbar)-->
+  <nav class="navbar navbar-light bg-light">
+    <a href="index.php" class="navbar-brand mb-0 h1">Craft Beers</a>
+  </nav>
+  <!--Final do navbar-->
+  <main class="content sem-sidebar">
+    <div class="card card-padding">
+      <!-- Formulário de Cadastro -->
+      <form action="post-cadastro.php" method="POST">
+        <h2 class="form-title">CADASTRO</h2>
+        <div class="row form-field">
+          <div class="col">
+            <input type="text" name="nome" class="form-control" placeholder="Nome*" maxlenght="50" required>
+          </div>
+        </div>
+        <div class="row form-field">
+          <div class="col-6">
+            <input type="date" name="data_nasc" class="form-control" placeholder="Data de Nascimento*" required>
+          </div>
+          <div class="col-6">
+            <input type="text" name="num_telefone" class="form-control" placeholder="Telefone*" maxlenght="20" required>
+          </div>
+        </div>
+        <div class="row form-field">
+          <div class="col">
+            <input type="email" name="email" class="form-control" placeholder="Email*" maxlenght="50" required>
+          </div>
+        </div>
+        <div class="row form-field">
+          <div class="col">
+            <input type="password" name="senha" class="form-control" placeholder="Senha*" maxlenght="15" required>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <button type="submit" class="btn btn-sucesso btn-outline-primary">Cadastrar</button>
+        </div>
+      </form>
+    </div>
+  </main>
+
+  <!-- JQuery, Bootstrap -->
+  <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ criar-categoria-post.php
@@ -0,0 +1,25 @@
+<?php
+
+  session_start();
+  include('verifica_login.php');
+  include('conexao.php');
+
+  // criar uma variavel que recebe os dados da sessao
+  $dados = $_SESSION['user'];
+
+  $descricao = mysqli_real_escape_string($conexao, $_POST['descricao']);
+
+  $query = "INSERT INTO categoria (descricao, usuario_id) VALUES ('{$descricao}', '{$dados['id']}');";
+
+  $result = mysqli_query($conexao, $query);
+
+  if ($result) {
+    header('Location: lista-categorias.php');
+    exit();
+  } else {
+    // Mostrar erro 
+    header('Location: criar-categoria.html');
+    exit();
+  }
+
+?>
